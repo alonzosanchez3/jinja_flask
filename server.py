8a63cc6544f3f2740eeb792eb62039792aa08d20
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-03-04 10:56:12 -0600

    using dynamic code to update footer

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, template_rendered, render_template
 from dotenv import load_dotenv
+import datetime
 import random
 
 load_dotenv()
@@ -9,7 +10,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   random_number = random.randint(1,10)
-  return render_template('index.html', num = random_number)
+  year = datetime.date.today().year
+  print(year)
+  return render_template('index.html', num = random_number, year=year)
 
 if(__name__ == '__main__'):
   app.run(debug=True, port=8000)
