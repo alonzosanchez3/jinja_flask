29978dd16a9566929bfd841c68a8da9f6deeea32
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-03-18 10:56:12 -0500

    used requests module to send request to genderize api

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, template_rendered, render_template
 from dotenv import load_dotenv
 import datetime
 import random
+import requests
 
 load_dotenv()
 
@@ -13,6 +14,14 @@ def home():
   year = datetime.date.today().year
   print(year)
   return render_template('index.html', num = random_number, year=year)
+
+@app.route('/guess/<name>')
+def guess_name(name):
+  response = requests.get(f'https://api.genderize.io?name={name}')
+  response.raise_for_status()
+  data = response.json()
+  return render_template('guess.html', data=data)
+
 
 if(__name__ == '__main__'):
   app.run(debug=True, port=8000)
